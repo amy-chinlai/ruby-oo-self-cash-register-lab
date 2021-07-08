c83209dda7619ba57a325fe66b8ceefcbe66748c
amy-chinlai <amy@actionnetwork.org>
2021-07-08 16:45:04 -0400

    commit tree 5f96fb5a43c2a2a90d2517a4ffbb748cca6f342d

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require 'pry'
+class CashRegister
 
+    attr_accessor :total, :discount, :title, :price
+
+    def initialize(discount = 0)
+        @total = 0
+        @items = []
+        @prices = []
+        @discount = discount
+    end
+
+    def add_item(title, price, quantity = 1)
+        @total += price * quantity
+        (1..quantity).collect do |item|
+            @items << title
+            @prices << price
+            @last_transaction_amount = price * quantity
+        end
+    end
+
+    def apply_discount
+        if @discount == 0
+            "There is no discount to apply."   
+        else 
+            @total = @total - (@discount * @total / 100)
+            "After the discount, the total comes to $#{@total}."
+        end
+    end
+
+    def items
+        @items
+    end
+
+    def void_last_transaction
+        @total -= @last_transaction_amount
+    end
+
+end
